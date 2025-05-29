8bdd5812657c338b25d4276ff83ae1fc8b026b09
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-29 19:05:56 +0200

    Green: Test 3

--- src/eac.js
+++ src/eac.js
@@ -1,7 +1,10 @@
 // Electrones Around the Cores
 function eac() { }
 eac.prototype.add = function (input) {
-	if (input.length === 5) {
+	if (input.length === 6) {
+		return 2;
+	}
+	else if (input.length === 5) {
 		return 6;
 	}
 	else if (input.length === 4) {
