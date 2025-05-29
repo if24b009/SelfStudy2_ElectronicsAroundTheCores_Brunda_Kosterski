6aae8cbc9fbbd919410f20213bc0db2a14b50157
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-29 18:50:30 +0200

    Red: Test 2

--- spec/eac_spec.js
+++ spec/eac_spec.js
@@ -6,4 +6,8 @@ describe('Electrones Around the Cores', function() {
 	it('should return 6 for [ 1, 2, 3, 4, 5 ]', function() {
 		expect(eac.add('[ 1, 2, 3, 4, 5 ]')).toEqual(6);
 	});
+
+	it('should return 4 for [ 2, 2, 3, 3 ]', function() {
+		expect(eac.add('[ 2, 2, 3, 3 ]')).toEqual(4);
+	});
 });
