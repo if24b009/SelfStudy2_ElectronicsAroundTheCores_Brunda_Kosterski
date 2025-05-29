8a6d6bcdb4100311bd17a106691a7f9c84ec33f5
k7d <62010853+Many-Attempts@users.noreply.github.com>
2025-05-29 19:04:16 +0200

    Red: Test 3

--- spec/eac_spec.js
+++ spec/eac_spec.js
@@ -12,4 +12,8 @@ describe('Electrones Around the Cores', function() {
     it('should return 4 for [ 2, 2, 3, 3 ]', function() { 
         expect(eacInstance.add([ 2, 2, 3, 3 ])).toEqual(4); 
     }); 
+
+	it('should return 2 for [ 6, 6, 4, 4, 1, 3 ]', function() { 
+        expect(eacInstance.add([ 6, 6, 4, 4, 1, 3 ])).toEqual(2); 
+    }); 
 });
