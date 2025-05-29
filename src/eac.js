7571590608df68d8b1f4eb6f2f5b4b6b78f1465c
Lisa Brunda <if24b009@technikum-wien.at>
2025-05-29 19:01:39 +0200

    Refactor: Test 2

--- src/eac.js
+++ src/eac.js
@@ -1,10 +1,10 @@
 // Electrones Around the Cores
-function eac() {}
-eac.prototype.add = function(string_input) {
-	if(string_input.length === 5){
+function eac() { }
+eac.prototype.add = function (input) {
+	if (input.length === 5) {
 		return 6;
 	}
-	else if(string_input.length === 4){
+	else if (input.length === 4) {
 		return 4;
 	}
 	return 0;
